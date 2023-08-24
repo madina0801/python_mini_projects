c79bf2ef71da779d70f015dd03bf4ee4edbbbe02
Madina <madinatussupova0820@gmail.com>
2023-08-24 13:58:22 +0600

    user guess is above or below the number

--- guess_my_number.py
+++ guess_my_number.py
@@ -1,6 +1,7 @@
 import random
 
 guess_range = input("Type a number: ")
+guesses = 0
 
 if guess_range.isdigit():
     guess_range = int(guess_range)
@@ -15,16 +16,21 @@ else:
 random_num = random.randint(0, guess_range)
 
 while True:
+    guesses += 1
     user_guess = input("Make a guess: ")
     if user_guess.isdigit():
         user_guess = int(user_guess)
-        if user_guess <= 0:
-            print("Enter a positive number!")
-            quit()
     else:
         print("Enter a number!")
-        quit()
-        
+        continue
 
+    if user_guess == random_num:
+        print("You got it right!")
+        break
+    else:
+        if user_guess > random_num:
+            print("You were above the number!")
+        else:
+            print("You were below the number!")
 
-print(random_num)
+print("You got it in", guesses, "guesses")
