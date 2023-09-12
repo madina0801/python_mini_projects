5ec0cdf57bd8396224685e09525e7f67553fd090
Madina <madinatussupova0820@gmail.com>
2023-09-12 14:26:12 +0600

    choose a mode

--- password_manager.py
+++ password_manager.py
@@ -1,3 +1,14 @@
 password = input("What is the master password? ")
 
-mode = input("Would you like to add a new password or view existing ones? (view/add) ")
+mode = input("Would you like to add a new password or view existing ones? (view/add) or q to quit ").lower()
+
+while True:
+	if mode == 'q':
+		quit
+	elif mode == 'view':
+		pass
+	elif mode == 'add':
+		pass
+	else:
+		print("Invalid mode!")
+		continue
